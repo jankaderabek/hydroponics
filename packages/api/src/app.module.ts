bb9026846b2b79e1bbe997fa92b3ba1a242e2a5f
Jan Kaderabek <kaderabek.jan@gmail.com>
2021-12-28 20:37:58 +0100

    [API-outputs] fix eslint errors

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { OutputsModule } from './outputs/outputs.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { OutputsModule } from './outputs/outputs.module'
 
 @Module({
   imports: [OutputsModule],
