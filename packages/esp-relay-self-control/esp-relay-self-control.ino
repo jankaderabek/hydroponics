0ad6265f4751d82f3ad04145bc080968312a7be2
Jan Kaderabek <kaderabek.jan@gmail.com>
2022-01-07 22:18:46 +0100

    [ESP-module] mDNS for discovery by DNS name instead of IP

--- packages/esp-relay-self-control/esp-relay-self-control.ino
+++ packages/esp-relay-self-control/esp-relay-self-control.ino
@@ -3,6 +3,9 @@
 #include <WiFiUdp.h>
 #include <ArduinoJson.h>
 #include <TimeLib.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncJson.h>
+#include <ESP8266mDNS.h>
 #include "esp-secrets.h"
 #include "ThingSpeak.h"
 
@@ -12,7 +15,7 @@ const char * thingSpeakWriteAPIKey = SECRET_WRITE_APIKEY;
 const char* ssid = SECRET_SSID;
 const char* password = SECRET_PASS;
 
-
+AsyncWebServer webServer(80);
 WiFiClient client;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, 3600);
@@ -22,7 +25,11 @@ const int mainLightOutput = D1;
 const int seedLightOutput = D2;
 
 bool lightsEnabled = false;
+bool seedsLightsEnabled = false;
 bool waterPumpEnabled = false;
+
+bool lightShouldBeEnabled = false;
+bool waterPumpShouldBeEnabled = false;
 
 const int RELAY_ENABLED = LOW;
 const int RELAY_DISABLED = HIGH;
@@ -64,9 +71,58 @@ void setup() {
   setSyncInterval(30);
 
   ThingSpeak.begin(client);
+
+  if (!MDNS.begin("hydroponics-relay-control")) {
+    Serial.println("Error setting up MDNS responder!");
+  }
+
+  MDNS.addService("http", "tcp", 80);
+
+  webServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", "Connected to WiFi network with IP Address: " + WiFi.localIP().toString());
+  });
+
+  webServer.on("/outputs", HTTP_GET, [](AsyncWebServerRequest *request){
+    sendStatusResponse(request);
+  });
+
+  AsyncCallbackJsonWebHandler* handler = new AsyncCallbackJsonWebHandler(
+    "/outputs",
+    [](AsyncWebServerRequest* request, JsonVariant& json) {
+      if (not json.is<JsonObject>()) {
+        request->send(400, "text/plain", "Not an object");
+        return;
+      }
+
+      auto&& data = json.as<JsonObject>();
+
+      if (data["waterPump"].is<bool>()) {
+        waterPumpEnabled = data["waterPump"];
+      }
+
+      if (data["mainLights"].is<bool>()) {
+        lightsEnabled = data["mainLights"];
+      }
+
+      if (data["seedsLights"].is<bool>()) {
+        seedsLightsEnabled = data["seedsLights"];
+      }
+
+      processRelayOutputs();
+      sendStatusResponse(request);      
+    }
+  );
+
+  handler->setMethod(HTTP_PATCH);
+  webServer.addHandler(handler);
+  
+  webServer.onNotFound(notFound);
+  webServer.begin();
 }
 
 void loop() {
+  MDNS.update();
+
   if ((millis() - lastTime) > timerDelay) {
     timeClient.update();
     Serial.printf("NTP time: %s\n", timeClient.getFormattedTime());
@@ -83,6 +139,22 @@ void loop() {
   }
 }
 
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+void sendStatusResponse (AsyncWebServerRequest* request) {
+  AsyncResponseStream *response = request->beginResponseStream("application/json");
+  DynamicJsonDocument doc(1024);
+
+  doc["waterPump"] = waterPumpEnabled;
+  doc["mainLights"] = lightsEnabled;
+  doc["seedsLights"] = seedsLightsEnabled;
+
+  serializeJson(doc, *response);
+  request->send(response);
+}
+
 time_t getNtpTime()
 {
   return timeClient.getEpochTime();
@@ -91,21 +163,30 @@ time_t getNtpTime()
 void processRelayOutputs () {
   const int currentHours = hour();
   const int currentMinutes = minute();
-  const bool lightsEnabledBefore = lightsEnabled;
-  const bool waterPumpEnabledBefore = waterPumpEnabled;
+  const bool lightsEnabledBefore = lightShouldBeEnabled;
+  const bool waterPumpEnabledBefore = waterPumpShouldBeEnabled;
 
-  lightsEnabled = shouldEnableLights(currentHours);
-  waterPumpEnabled = shouldEnableWaterPump(currentHours, currentMinutes);
+  lightShouldBeEnabled = shouldEnableLights(currentHours);
+  waterPumpShouldBeEnabled = shouldEnableWaterPump(currentHours, currentMinutes);
+
+  if (lightShouldBeEnabled != lightsEnabledBefore) {
+    lightsEnabled = lightShouldBeEnabled;
+    seedsLightsEnabled = lightShouldBeEnabled;
+  }
+
+  if (waterPumpShouldBeEnabled != waterPumpEnabledBefore) {
+    waterPumpEnabled = waterPumpShouldBeEnabled;
+  }
 
   Serial.printf("Current time is %d:%d\n", hour(), minute());
   Serial.printf("Lights enabled: %d\n", lightsEnabled);
   Serial.printf("Water pump enabled %d\n", waterPumpEnabled);
 
   digitalWrite(seedLightOutput, lightsEnabled ? RELAY_ENABLED : RELAY_DISABLED);
-  digitalWrite(mainLightOutput, lightsEnabled ? RELAY_ENABLED : RELAY_DISABLED);
+  digitalWrite(mainLightOutput, seedsLightsEnabled ? RELAY_ENABLED : RELAY_DISABLED);
   digitalWrite(waterPumpOutput, waterPumpEnabled ? RELAY_ENABLED : RELAY_DISABLED);
 
-  if (lightsEnabledBefore != lightsEnabled || waterPumpEnabledBefore != waterPumpEnabled) {
+  if (lightsEnabledBefore != lightShouldBeEnabled || waterPumpEnabledBefore != waterPumpShouldBeEnabled) {
     updateThingSpeak();
   }
 }
