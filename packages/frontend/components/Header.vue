3595028534db8b4153519e9b24a9b636296f02a9
Jan Kaderabek <kaderabek.jan@gmail.com>
2022-01-08 22:35:08 +0100

    [frontend] Remove icons because of SSR problems

--- packages/frontend/components/Header.vue
+++ packages/frontend/components/Header.vue
@@ -17,8 +17,7 @@
           <!-- Mobile menu button -->
           <DisclosureButton class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
             <span class="sr-only">Open main menu</span>
-            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+              Menu
           </DisclosureButton>
         </div>
       </div>
@@ -33,8 +32,7 @@
 </template>
 
 <script setup lang="ts">
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
 
 const user = {
   name: 'Tom Cook',
