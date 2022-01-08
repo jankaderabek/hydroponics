9da9b907aced0d9c1783eb52bfda1af468106471
Jan Kaderabek <kaderabek.jan@gmail.com>
2022-01-08 20:43:05 +0100

    [frontend] Statis list of outputs

---
+++ packages/frontend/components/OutputState.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="max-w-7xl mx-auto my-3 sm:px-6 lg:px-8">
+    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div class="px-4 py-5 sm:px-6 flex items-center">
+        <div class="w-8 h-8 rounded-full" :class="{ 'bg-red-600': !active, 'bg-green-600': active } "></div>
+
+        <h3 class="text-lg leading-6 font-medium text-gray-900 px-4">
+          {{ title }}
+        </h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  active: Boolean,
+  title: String
+})
+</script>
--- packages/frontend/pages/index.vue
+++ packages/frontend/pages/index.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
-    <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-gray-900">
-          Dashboard
-        </h1>
-      </div>
-    </header>
-
     <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        List of outputs
-      </div>
+      <OutputState title="Hlavní světla"/>
+
+      <OutputState title="Čerpadlo" :active="true"/>
     </main>
   </div>
 </template>
+
+<script setup lang="ts">
+import OutputState from "~/components/OutputState.vue"
+</script>
